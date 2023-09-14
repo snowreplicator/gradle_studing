e13008abb6a21e4734a7e15672f53eba87aa7af1
Zorro <zorro@nomail.ru>
2023-09-14 20:56:58 +0300

    second app

---
+++ simple_app_2/src/main/java/ru/zorro/app2/TestApp2.java
@@ -0,0 +1,17 @@
+package ru.zorro.app1;
+
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+
+public class TestApp2 {
+
+    public static void main(String[] args) {
+        System.out.println("\n------\nstart TestApp2\n");
+        LocalDate currentDate = LocalDate.now();
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("currentDate: " + currentDate);
+        System.out.println("currentTime: " + currentTime);
+        System.out.println("\nstop TestApp2\n------\n");
+    }
+
+}
